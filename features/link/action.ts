df8e28191e23def33c25b1da3cdaa1d1a2dab494
maia2a <gabrielldeveloper@gmail.com>
2025-08-05 13:48:23 -0300

    feat: implement createLinkAction and deleteLinkAction functions with validation using Zod

---
+++ features/link/action.ts
@@ -0,0 +1,39 @@
+"use server";
+import { createClient } from "@/lib/supabase/client";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+
+const linkSchema = z.object({
+  title: z.string().min(1, "Title is required"),
+  url: z.string().url("Invalid URL").min(1, "URL is required"),
+});
+
+export async function createLinkAction(formData: FormData) {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    throw new Error("User not authenticated");
+  }
+
+  const validateFields = linkSchema.safeParse({
+    title: formData.get("title"),
+    url: formData.get("url"),
+  });
+  if (!validateFields.success) {
+    return { error: validateFields.error };
+  }
+  const { error } = await supabase.from("links").insert({
+    title: validateFields.data.title,
+    url: validateFields.data.url,
+    user_id: user.id,
+  });
+  if (error) {
+    return { error: error.message };
+  }
+  revalidatePath("/dashboard");
+}
+
+export async function deleteLinkAction(linkId: string) {}
